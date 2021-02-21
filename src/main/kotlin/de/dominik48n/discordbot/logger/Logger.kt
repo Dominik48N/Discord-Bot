a92566b9d59c30d4174e4aebc010b9a342b7622b
Dominik48N <yt.dominik48n@gmail.com>
2021-02-21 17:33:48 +0100

    Bug fix: If Error and Warning messages are displayed in the console, only 1 space is displayed from now on.

--- src/main/kotlin/de/dominik48n/discordbot/logger/Logger.kt
+++ src/main/kotlin/de/dominik48n/discordbot/logger/Logger.kt
@@ -6,8 +6,8 @@ package de.dominik48n.discordbot.logger
 enum class Logger( private val prefix: String ) {
 
     INFO( "[INFO]" ),
-    ERROR( "[ERROR] " ),
-    WARNING( "[WARNING] " );
+    ERROR( "[ERROR]" ),
+    WARNING( "[WARNING]" );
 
     fun print( message: String ) {
         println( this.prefix + " " + message )
